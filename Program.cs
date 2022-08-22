93aa6b48eb02b6ea2fcb43784686d5fbffb313c6
jyoti <jyotidhage10@gmail.com>
2022-08-22 16:37:49 +0530

    [Aaradhya]Add:Ability to create Binary Search tree

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BinarySearchTree1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("---------Welcome To Binary Search Tree--------");
+            Console.WriteLine();
+            BST<int> bST = new BST<int>(56);
+            bST.Insert(30);
+            bST.Insert(70);
+            bST.Display();
+           
+
+            Console.Read();
+        }
+    }
+}
